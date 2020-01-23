f3be2c2edfd6154df16f52e40fb6bbd920b1c684
Fagan Safarov <fagansafarov1985@gmail.com>
2020-01-22 20:35:46 -0600

    latest Studio for junit

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,9 +8,57 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void noBracket() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void openingBracket() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+
+    @Test
+    public void closingBracket() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("Launch]Code"));
+    }
+    @Test
+    public void onlyPairBracket() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void reverseOrderBracket() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void notMatchingPairBracket() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("Laun[ch]Cod]e"));
+    }
+    @Test
+    public void moreMatchingBracket() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("Laun[ch]Cod]e["));
+    }
+    @Test
+    public void curlyBracket() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("Laun{ch}Code"));
+    }
+
+    @Test
+    public void oneCurlyOneSquareBracket() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("Laun[ch{Code"));
+    }
+
+    @Test
+    public void moreBracketsTypes() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("Lau()n[ch{Cod][})e"));
     }
 
 
